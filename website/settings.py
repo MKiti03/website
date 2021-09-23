d709da985526737a0a23c8c4c275b8b12bef3e96
Sonfack Nelson Mandela <patricknelson1995@gmail.com>
2021-09-23 13:05:39 +0100

    firt deplay

--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=34b_m_=t)p7@!$(0v(zxttudc*8diz@+66#7_@2g-i@*$s@!j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://vecademy-web-site.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['studyabroad.vecademy.com', '138.197.183.144']
 
 
 # Application definition
@@ -87,10 +87,21 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vecademy_consulting',
+        'USER': 'vecademy',
+        'PASSWORD': 'vecademy@CONSULTING#2021',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
