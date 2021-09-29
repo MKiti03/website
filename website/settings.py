1727f126124600346d3f38eb2dd91badfc51f5e6
Sonfack Nelson Mandela <patricknelson1995@gmail.com>
2021-09-29 08:22:16 +0100

    To try deplay

--- website/settings.py
+++ website/settings.py
@@ -143,8 +143,8 @@ STATICFILES_DIRS = [
 ]
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'static/madia'
+MEDIA_URL = '/images/'
+MEDIA_ROOT = BASE_DIR / 'static/images'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
