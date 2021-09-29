40730e205845603c867ff6bf4e9750e3ef3f5a90
Sonfack Nelson Mandela <patricknelson1995@gmail.com>
2021-09-29 09:40:43 +0100

    To try deplay

--- website/settings.py
+++ website/settings.py
@@ -88,23 +88,23 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'vecademy_consulting',
-#         'USER': 'vecademyuser',
-#         'PASSWORD': 'vecademy@CONSULTING#2021',
-#         'HOST': 'localhost',
-#         'PORT': '',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vecademy_consulting',
+        'USER': 'vecademyuser',
+        'PASSWORD': 'vecademy@CONSULTING#2021',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
